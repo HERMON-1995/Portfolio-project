cc081c3ac24eefe09a6ee4cad3b441bf19dec522
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-01-31 00:57:56 +0300

    Update HTML, CSS and JS

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav');
+const logo = document.querySelector('.logo');
+const floating = document.querySelector('.header');
+
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('is-active');
   mobileMenu.classList.toggle('is-active');
+  logo.classList.toggle('removed');
+  floating.classList.toggle('float');
 });
