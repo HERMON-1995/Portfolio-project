9003aad81aee119f0fbfb99dadb853f951d846ce
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-01-30 21:31:59 +0300

    Update Html and fix errors

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav')
+const mobileMenu = document.querySelector('.mobile-nav');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    mobile_menu.classList.toggle('is-active');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-active');
 });
