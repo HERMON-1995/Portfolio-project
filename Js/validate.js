f3667607135bcf08176dddbfee730ac2b2373ff0
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-01 18:22:25 +0300

    Update JS file

--- Js/validate.js
+++ Js/validate.js
@@ -1,32 +1,19 @@
-const formbutton= document.getElementById('btnid');
+const formbutton = document.getElementById('btnid');
 
 function validate() {
-    const text = document.getElementById('email').value;
-    const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-    document.forms[0].onsubmit = function email(e) {
-      if (!(validator.test(text))) {
-        document.getElementById('error-message').innerHTML = 'Please use lowercase to enter valid email address!';
-        document.getElementById('error-message').style.visibility = 'visible';
-        document.getElementById('error-message').style.color = 'white';
-        document.getElementById('error-message').style.background = 'red';
-        document.getElementById('error-message').style.textAlign = 'center';
-        e.preventDefault();
-      }
-    };
-  }
-  formbutton.addEventListener('click', () => {
-    validate();
-  });
-
-
-
-
-
-
-
-{/*#error-message {
-    position: relative;
-    top: -123px;
-    border-radius: 8px;
-    font-size: 15px;
-  }*/}
+  const emailText = document.getElementById('email').value;
+  const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  document.forms[0].onsubmit = function email(e) {
+    if (!(validator.test(emailText))) {
+      document.getElementById('error-message').innerHTML = 'Please use lowercase letters only for a valid email address!';
+      document.getElementById('error-message').style.visibility = 'visible';
+      document.getElementById('error-message').style.color = 'red';
+      document.getElementById('error-message').style.background = 'white';
+      document.getElementById('error-message').style.textAlign = 'center';
+      e.preventDefault();
+    }
+  };
+}
+formbutton.addEventListener('click', () => {
+  validate();
+});
