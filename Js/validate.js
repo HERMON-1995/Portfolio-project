4f5d1bb826e859e9cf6a9aa93ef0749a3d82aaef
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-02 16:09:37 +0300

    Update validate.js

--- Js/validate.js
+++ Js/validate.js
@@ -2,10 +2,10 @@ const formbutton = document.getElementById('btnid');
 
 function validate() {
   const emailText = document.getElementById('email').value;
-  const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  //const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
   document.forms[0].onsubmit = function email(e) {
     if (!(validator.test(emailText))) {
-      document.getElementById('error-message').innerHTML = 'Please use lowercase letters only for a valid email address!';
+     // document.getElementById('error-message').innerHTML = 'Please use lowercase letters only for a valid email address!';
       document.getElementById('error-message').style.visibility = 'visible';
       document.getElementById('error-message').style.color = 'red';
       document.getElementById('error-message').style.background = 'white';
@@ -15,5 +15,5 @@ function validate() {
   };
 }
 formbutton.addEventListener('click', () => {
-  validate();
+ // validate();
 });
