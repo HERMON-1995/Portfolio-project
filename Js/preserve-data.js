e2e4dc9dc05f25c38b9d62bf934baeee698cf055
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-02-02 12:32:25 +0300

    Update preserve-data.js file

--- Js/preserve-data.js
+++ Js/preserve-data.js
@@ -1,22 +1,21 @@
-let datas = [];
+const datas = [];
 
-const addData = (ev)=> {
-    ev.preventDefault();
-    let data = {
-        id: Date.now(),
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    }
-    datas.push(data);
-    document.forms[0].reset();
+const addData = (ev) => {
+  ev.preventDefault();
+  const data = {
+    id: Date.now(),
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+  };
+  datas.push(data);
+  document.forms[0].reset();
 
-    console.warn('Adjected', {datas});
+  console.warn('Adjected', { datas });
 
-      localStorage.setItem('InputDataList', JSON.stringify(datas));
+  localStorage.setItem('InputDataList', JSON.stringify(datas));
+};
 
-}
-
-    document.addEventListener('DOMContentLoaded', ()=> {
-       document.getElementById('btnid').addEventListener('click', addData); 
-    })
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btnid').addEventListener('click', addData);
+});
