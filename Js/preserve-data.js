5f676e713d9cb630213fc9414bc33de2e3e4e190
Ashna Ali <116655378+Ashnaali3255@users.noreply.github.com>
2023-02-02 17:42:04 +0500

    Update preserve-data.js

--- Js/preserve-data.js
+++ Js/preserve-data.js
@@ -13,3 +13,10 @@ form.addEventListener('submit', (ev) => {
 });
 const retrievedObject = localStorage.getItem('InputDataList');
 const getValue = JSON.parse(retrievedObject);
+window.addEventListener('load', () => {
+  if (localStorage.getItem('InputDataList')) {
+    document.getElementById('name').value = getValue.name;
+    document.getElementById('email').value = getValue.email;
+    document.getElementById('message').value = getValue.message;
+  }
+});
